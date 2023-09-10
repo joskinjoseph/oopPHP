848418b74fd85ed7abf75522b6562dddd7df08e7
Akinbomi Oluwadamilare <joskinjoseph1@gmail.com>
2023-09-10 22:44:39 +0100

    Final Method and Class

--- new/new o/index.php
+++ new/new o/index.php
@@ -5,37 +5,20 @@ ini_set('display_errors', 1);
 
 class User
 {
-    private $name;
-    private $age;
+    public $name = "Joseph";
+    public $age;
 
-    public function __construct($name, $age)
-    {
-        $this->name = $name;
-        $this->age = $age;
-    }
-
-    public function __toString()
-    {
-        return "Name: " . $this->name . "<br> Age: " . $this->age;
-    }
-
-    public function setName($name) {
-        $this->name = $name;
-    }
-
-    public function getName(){
-        echo $this->name;
-    }
-
-    public function setAge($age) {
-        $this->name = $age;
-    }
-
-    public function getAge(){
-        echo $this->age;
+    final public function printName($name){
+        return "My name is " . $name;
     }
 
 }
 
-$user = new User("Joskin", 25);
-echo $user;
+class Manager extends User {
+    public $name = "Joskin";
+
+    
+}
+
+$manager = new Manager();
+echo $manager->printName("JOSKIN");
