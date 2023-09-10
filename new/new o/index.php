e3c165c5f8131bfeb269ccce3c98bc80e690bf32
Akinbomi Oluwadamilare <joskinjoseph1@gmail.com>
2023-09-10 21:46:28 +0100

    Constructor and deconstructor

--- new/new o/index.php
+++ new/new o/index.php
@@ -1,29 +1,54 @@
 <?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 class User {
-    public $name;
-    public $email;
+    private $name;
+    private $age;
 
-    public function welcomeMessage() {
-        return "Have a good working day";
+    public function __construct($name, $age) {
+        $this->name = $name;
+        $this->age = $age;
     }
+
+    public function getName() {
+       return $this->name;
+    }
+
+    public function setName($name) {
+       $this->name = $name;
+     }
+
+    public function getAge() {
+        return $this->age;
+     }
+ 
+     public function setAge($age) {
+        $this->age = $age;
+      }
+
 }
 
-class Admin extends User {
-    public $level;
-}
 
-$user = new User();
-$user->name = "Joskin";
-$user->email = "joskin@example.com";
-echo $user->name . " " . $user->email;
-
-
-
-$admin = new Admin();
-$admin->name = "Man";
-$admin->email = "men@example.com";
-$admin->level = "Super Level";
-echo $admin->name . " " . $admin->email.  " " . $admin->level;
+$user = new User("Dary", 24);
+echo $user->getName();
 echo "<hr>";
-echo $admin->welcomeMessage();
+echo $user->getAge();
+
+
+
+// $user->name = "Joskin";
+// $user->email = "joskin@example.com";
+// echo $user->name . " " . $user->email;
+
+
+
+// $admin = new Admin();
+// $admin->name = "Man";
+// $admin->email = "men@example.com";
+// $admin->level = "Super Level";
+// echo $admin->name . " " . $admin->email.  " " . $admin->level;
+// echo "<hr>";
+// echo $admin->welcomeMessage();
 
